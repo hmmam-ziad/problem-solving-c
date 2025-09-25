999253fd7cdea3645e4204ad2abddf33f7490fe2
hmmam-ziad <hmmamxxc@gmail.com>
2025-09-25 19:33:42 +0300

    displays all digits, on a single line, by ascending order

---
+++ 42Modules/Imperative-Programming/C-Programming-Fundamentals/ft_print_numbers.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void ft_print_numbers(void);
+
+int main()
+{
+    ft_print_numbers();
+    return 0;
+}
+
+void ft_print_numbers()
+{
+    char c = '0';
+    while (c <= '9')
+    {
+        write(1, &c, 1);
+        c++;
+    }
+}
