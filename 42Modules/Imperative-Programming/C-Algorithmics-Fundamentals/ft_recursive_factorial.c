cf3028ac7c43c8042e32ed067811ade33b0e7b30
hmmam-ziad <hmmamxxc@gmail.com>
2025-09-27 12:19:27 +0300

    recursive_factorial

---
+++ 42Modules/Imperative-Programming/C-Algorithmics-Fundamentals/ft_recursive_factorial.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+int ft_recursive_factorial(int nb);
+void ft_putchar(char c);
+void ft_putnbr(int n);
+
+int main()
+{
+    ft_putnbr(ft_recursive_factorial(5));
+    return 0;
+}
+
+int ft_recursive_factorial(int nb)
+{
+    
+    if (nb < 0)
+        return (0);
+    if (nb == 0 || nb == 1)
+        return (1);
+    return (nb * ft_recursive_factorial(nb - 1));
+}
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_putnbr(int n)
+{
+    if(n >= 10)
+    {
+        ft_putnbr(n / 10);
+    }
+    ft_putchar(n % 10 + '0');
+}
