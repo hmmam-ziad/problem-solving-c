8a5d4d0888c45024f28eba35847f1156d46b5cd3
hmmam-ziad <hmmamxxc@gmail.com>
2025-09-27 12:59:32 +0300

    function which reverses a given array of integer

---
+++ 42Modules/Memory-Management/C-Pointers/ft_rev_int_tab.c
@@ -0,0 +1,17 @@
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	temp;
+	int	start;
+	int	end;
+
+	start = 0;
+	end = size - 1;
+	while (start < end)
+	{
+		temp = tab[start];
+		tab[start] = tab[end];
+		tab[end] = temp;
+		start++;
+		end--;
+	}
+}
