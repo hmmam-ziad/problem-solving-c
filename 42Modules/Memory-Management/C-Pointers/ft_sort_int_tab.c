55efad1cca0c8ccd63e91c9d21facf1404cde0ce
hmmam-ziad <hmmamxxc@gmail.com>
2025-09-27 13:01:27 +0300

    function which sorts an array of integers by ascending order

---
+++ 42Modules/Memory-Management/C-Pointers/ft_sort_int_tab.c
@@ -0,0 +1,20 @@
+void ft_sort_int_tab(int *tab, int size)
+{
+
+    int i;
+    int j;
+    int temp;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - 1 - i; j++)
+        {
+            if (tab[j] > tab[j + 1])
+            {
+                temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+            }
+        }
+    }
+}
